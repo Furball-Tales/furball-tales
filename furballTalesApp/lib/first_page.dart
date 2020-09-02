89dc551c859960404da1cf2209aa15cefe5922dd
namitry <nobo918@gmail.com>
2020-09-02 18:36:08 +0900

    combined google auth with homepage

--- furballTalesApp/lib/first_page.dart
+++ furballTalesApp/lib/first_page.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(color: Colors.blue[100]),
-    );
-  }
-}
+// class FirstScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(color: Colors.blue[100]),
+//     );
+//   }
+// }
--- furballTalesApp/lib/login_page.dart
+++ furballTalesApp/lib/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'sign_in.dart';
-import 'first_page.dart';
+import 'homepage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
           Navigator.of(context).push(
             MaterialPageRoute(
               builder: (context) {
-                return FirstScreen();
+                return Homepage();
               },
             ),
           );
