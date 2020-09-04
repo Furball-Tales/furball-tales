0fc12bee2054f0c91da9496983138f7cfe201b74
namitry <nobo918@gmail.com>
2020-09-04 14:10:52 +0900

    started working on implementing gridView

---
+++ furballTalesApp/lib/grid_view.dart
@@ -0,0 +1,15 @@
+class GridView extends StatefulWidget {
+  GridView({Key key}) : super(key: key);
+
+  @override
+  _GridViewState createState() => _GridViewState();
+}
+
+class _GridViewState extends State<GridView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
